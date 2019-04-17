89f2205897e6ab845b55c34225a4a5b3080acb83
piotrbarc <43997971+piotrbarc@users.noreply.github.com>
2019-04-17 16:24:55 +0200

    First commit - generated some components

---
+++ ProjektAngular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    StartPageComponent,
+    AdminPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
