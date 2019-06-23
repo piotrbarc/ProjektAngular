10634039b1a73c398f18eb2b59e318da23d4e0fd
piotrbarc <43997971+piotrbarc@users.noreply.github.com>
2019-06-23 17:49:48 +0200

    Back to projet ....

--- ProjektAngular/src/app/app.component.ts
+++ ProjektAngular/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class AppComponent {
   title = 'ProjektAngular';
+
+
+  
+
 }
